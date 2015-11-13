511bc10b853ed99309d978158888686cd24b6960
Philippe Luchansky <philuchansky@gmail.com>
2015-11-13 11:27:13 -0800

    mongoose integration

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var mongoose = require('mongoose');
 var app = express();
 
 // STARTED ON BUT NOT TESTED
+mongoose.connect('connection-string-goes-here', function(){
+	console.log('connected to mongo!');
+})
 
 
 app.listen(3000, function(){
