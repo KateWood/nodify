d82428616a1cdd9533c5201c7be72bc3108f35f7
Philippe Luchansky <philuchansky@gmail.com>
2015-11-13 11:24:13 -0800

    mongo in progress

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
+
+// STARTED ON BUT NOT TESTED
+
 
 app.listen(3000, function(){
 	console.log('server running.')
